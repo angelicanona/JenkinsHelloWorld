462487d35807eec0147dd3e2ff38e320147841c6
angelicanona <85670936+angelicanona@users.noreply.github.com>
2021-08-12 11:18:49 +0800

    Adding new files

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Angel!");
+        int MyLastOne = 883;
+        System.out.println(MyLastOne);
+    }
+}
+
